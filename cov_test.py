ee1c8ed930f315e5fab7dfaebdddcca59104c7c3
shijingwei <535650995@qq.com>
2018-09-02 17:29:05 +0800

    学习程序

---
+++ cov_test.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+x=[1,2,3,4]
+print("mean:",np.mean(x))
+print("cov:",np.cov(x))
+
+x=[1,2,3,4]
+y=[1,1,1,1]
+
+# x = np.array([[1, 5, 6], [4, 3, 9], [4, 2, 9], [4, 7, 2]])
+print(np.cov(x,y))
